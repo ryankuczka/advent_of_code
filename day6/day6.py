2ef9e744efc3010356c40a585a3167a6a8c5c55c
Ryan Kuczka <ryan.kuczka@gmail.com>
2015-12-08 08:45:20 -0500

    Improve day 6 and use new class

--- day6/day6.py
+++ day6/day6.py
@@ -1,58 +1,51 @@
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, os.pardir, os.pardir)))
+
 import re
+from utils import AdventDay
 
 
-LINE_REGEX = re.compile(r'(toggle|turn (?:on|off)) (\d{,3}),(\d{,3}) through (\d{,3}),(\d{,3})')
+class Day6(AdventDay):
+    DAY = 6
 
-def get_input():
-    with open(os.path.abspath(os.path.join(__file__, os.pardir, 'input.txt'))) as input_file:
-        return [line.strip() for line in input_file.readlines()]
+    LINE_REGEX = re.compile(r'(toggle|turn (?:on|off)) (\d{,3}),(\d{,3}) through (\d{,3}),(\d{,3})')
 
+    def init_light_grid(self, init_value):
+        self.lights = [[init_value for x in range(1000)] for y in range(1000)]
 
-def part1():
-    lights = [[False for x in range(1000)] for y in range(1000)]
+    def toggle_lights(self):
+        for line in self.get_input():
+            instr, xstart, ystart, xend, yend = self.LINE_REGEX.match(line).groups()
+            for x in range(int(xstart), int(xend) + 1):
+                for y in range(int(ystart), int(yend) + 1):
+                    if instr == 'toggle':
+                        self.lights[x][y] = self.toggle(x, y)
+                    elif instr == 'turn on':
+                        self.lights[x][y] = self.on(x, y)
+                    elif instr == 'turn off':
+                        self.lights[x][y] = self.off(x, y)
 
-    for line in get_input():
-        instr, xstart, ystart, xend, yend = LINE_REGEX.match(line).groups()
-        for x in range(int(xstart), int(xend) + 1):
-            for y in range(int(ystart), int(yend) + 1):
-                if instr == 'toggle':
-                    lights[x][y] = not lights[x][y]
-                elif instr == 'turn on':
-                    lights[x][y] = True
-                elif instr == 'turn off':
-                    lights[x][y] = False
+    def part1(self):
+        self.init_light_grid(False)
+        self.toggle = lambda x, y: not self.lights[x][y]
+        self.on = lambda x, y: True
+        self.off = lambda x, y: False
 
-    light_count = 0
-    for row in lights:
-        for light in row:
-            light_count += 1 if light else 0
+        self.toggle_lights()
 
-    print '# OF LIGHTS ON:', light_count
+        return sum(x for y in self.lights for x in y)
 
+    def part2(self):
+        self.init_light_grid(0)
+        self.toggle = lambda x, y: self.lights[x][y] + 2
+        self.on = lambda x, y: self.lights[x][y] + 1
+        self.off = lambda x, y: max(0, self.lights[x][y] - 1)
 
-def part2():
-    lights = [[0 for x in range(1000)] for y in range(1000)]
+        self.toggle_lights()
 
-    for line in get_input():
-        instr, xstart, ystart, xend, yend = LINE_REGEX.match(line).groups()
-        for x in range(int(xstart), int(xend) + 1):
-            for y in range(int(ystart), int(yend) + 1):
-                if instr == 'toggle':
-                    lights[x][y] += 2
-                elif instr == 'turn on':
-                    lights[x][y] += 1
-                elif instr == 'turn off':
-                    lights[x][y] = max(0, lights[x][y] - 1)
-
-    brightness = 0
-    for row in lights:
-        for light in row:
-            brightness += light
-
-    print 'TOTAL BRIGHTNESS:', brightness
+        return sum(x for y in self.lights for x in y)
 
 
 if __name__ == '__main__':
-    part1()
-    part2()
+    Day6().run()
